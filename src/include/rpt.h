44657c6075a8fde22300b8084c1dc4e737df6c45
Jools Wills <jools@oxfordinspire.co.uk>
2015-03-02 09:07:55 +0000

    fix for missing fs_get_monotonic_time

--- src/include/rpt.h
+++ src/include/rpt.h
@@ -6,5 +6,25 @@
   * Copyright 1998 Bernd Schmidt
   */
 
-typedef unsigned long frame_time_t;
+#ifndef MACHDEP_RPT_H
+#define MACHDEP_RPT_H
 
+#include <sys/time.h>
+
+typedef unsigned long frame_time_t;
+static int64_t fs_get_monotonic_time(void);
+extern int64_t g_uae_epoch;
+
+static inline frame_time_t read_processor_time() {
+    // with frame_time_t being int, the value will wrap around in
+    // about 23 days..
+    return (fs_get_monotonic_time() - g_uae_epoch);
+}
+
+static int64_t fs_get_monotonic_time(void) {
+    struct timespec ts;
+    clock_gettime (CLOCK_MONOTONIC, &ts);
+    return (((int64_t) ts.tv_sec) * 1000000) + (ts.tv_nsec / 1000);
+}
+
+#endif // MACHDEP_RPT_H
